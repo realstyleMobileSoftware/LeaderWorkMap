0748cdaa5ab2740f069d45dff58dc8098aff949e
Rick_rui <jingrui.mao@gmail.com>
2015-04-01 15:12:31 +0800

    init
    初始化

---
+++ LeaderWorkMap/LeaderWorkMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LeaderWorkMap
+//
+//  Created by zhangjr on 15/4/1.
+//  Copyright (c) 2015年 RealStyle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LeaderWorkMap/LeaderWorkMap/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LeaderWorkMap
+//
+//  Created by zhangjr on 15/4/1.
+//  Copyright (c) 2015年 RealStyle. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
